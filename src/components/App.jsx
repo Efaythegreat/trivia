0a2af8d58411394f8e4924081afdaf7fd53c729c
Efay <65887283+Efaythegreat@users.noreply.github.com>
2022-01-26 17:48:18 -0800

    added question contents and a button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,33 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Questions(question) {
+  return <h1>{question.text}</h1>;
+}
+
+function NextQuestion(nQuestion) {
+  return <h2>{nQuestion.text}</h2>;
+}
+
+function AnswerOne(props) {
+  return <h1></h1>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  let question = 0;
+
+  return (
+    <div className="app">
+      Trivia!
+      <div>
+        <Questions text="Questions Goes Here" />
+      </div>
+      <div>
+        <NextQuestion text="The Next Questions Goes Here" />
+      </div>
+      <button>Cool Button</button>
+    </div>
+  );
 }
 
 export default App;
