9fcd0cb4e2ae93ee2c5375762402e33695c4a01a
Efay <65887283+Efaythegreat@users.noreply.github.com>
2022-02-23 18:16:11 -0800

    read me almost completed day 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,36 +7,67 @@ function Questions(question) {
 }
 
 function Answers(props) {
-  var x = [];
   let choices = props.answer;
-  console.log(props);
+  //console.log(props);
 
-  for (let i = 0; i < choices.length; i++) {
+  /* for (let i = 0; i < choices.length; i++) {
     x.push(<div>{choices[i]}</div>);
   }
+  */
+  let choice = choices.map((x) => (
+    <>
+      <div>{x}</div> <br />
+    </>
+  ));
+
   //x.push(<br/>)
 
-  console.log(x);
-  return <div>{x}</div>;
-}
-
-function NextQuestion(props) {
-  return <button>{props.text}</button>;
+  //console.log(choice);
+  return <div>{choice}</div>;
 }
 
 function App() {
-  let question = 0;
-  console.log(data[0].question.questionText);
+  var index = 0;
+  /*function indexGOBrr(){
+    index += 
+  }
+*/
+  // console.log(data[index].question.text);
+
+  function NextQuestion(props) {
+    return <button /*onClick={() => indexGoBrr  ()}*/>{props.text}</button>;
+  }
+
+  const [answerDisplayed, setAnswerDisplayed] = useState(false);
+
+  function showAnswer() {
+    let choices = data[index].question.choices;
+
+    for( let i = 0; i < choices.length; i++){
+      if (i === data[index].question.correct_choice_index){
+        setAnswerDisplayed(true);
+        console.log(answerDisplayed);
+
+        return choices[i]
+  }
+
+
+
   return (
     <div className="app">
       <h1>Trivia!</h1>
       <div>
-        <Questions text={data[0].question.questionText} />
+        <Questions text={data[index].question.text} />
 
-        <Answers answer={data[0].question.answerChoices} />
+        <Answers answer={data[index].question.choices} />
       </div>
       <div>
         <NextQuestion text="Next Question" />
+      </div>
+      <div>
+        <br />
+        <button onClick={() => showAnswer()}>Display Answer</button>
+        <div className="answer"></div>
       </div>
     </div>
   );
